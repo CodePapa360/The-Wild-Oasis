7e3ac47306bd771d1e2968ef73998c5d1fd5a818
codepapa <codepapa360@gmail.com>
2023-10-21 14:28:56 +0600

    setup styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const Button = styled.button`
+  background-color: var(--color-brand-600);
+  font-size: 2rem;
+  color: white;
+  cursor: pointer;
+  border: none;
+  border-radius: 5px;
+  margin: 3rem;
+  padding: 1rem;
+
+  &:hover {
+    background-color: var(--color-brand-800);
+  }
+`;
+
 function App() {
   return (
-    <div>
-      Hello world
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <Button onClick={() => alert("You clicked")}>Click</Button>
+    </>
+  );
 }
 
-export default App
+export default App;
