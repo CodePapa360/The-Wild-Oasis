602bd4e304be388e42d32611850de44505d22be3
codepapa <codepapa360@gmail.com>
2023-10-24 15:23:52 +0600

    updating app settings - functionalities

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+
+export function useUpdateSetting() {
+  const quryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      toast.success("Setting successfully updated");
+      quryClient.invalidateQueries({
+        queryKey: ["settings"],
+      });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isUpdating, updateSetting };
+}
