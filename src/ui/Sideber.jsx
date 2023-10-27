952609b47fc387ce97479cf1e6df25f428b79564
codepapa <codepapa360@gmail.com>
2023-10-27 23:28:54 +0600

    uploading sample data

--- src/ui/Sideber.jsx
+++ src/ui/Sideber.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 import Logo from "./Logo";
 import MainNav from "./MainNav";
+import Uploader from "../data/Uploader";
 
 const StyledSideber = styled.aside`
   background-color: var(--color-grey-0);
@@ -19,6 +20,8 @@ function Sideber() {
     <StyledSideber>
       <Logo />
       <MainNav />
+
+      <Uploader />
     </StyledSideber>
   );
 }
