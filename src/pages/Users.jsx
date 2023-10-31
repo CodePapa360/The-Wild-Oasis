e6a2b5340e63d003ba2aa9aa554dc52ca0456fa5
codepapa <codepapa360@gmail.com>
2023-10-31 21:24:26 +0600

    redeploy

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,11 @@
-import SignUpForm from "../features/authentication/SignUpForm";
+import SignupForm from "../features/authentication/SignUpForm";
 import Heading from "../ui/Heading";
 
 function NewUsers() {
   return (
     <>
       <Heading as="h1">Create a new user</Heading>
-      <SignUpForm />
+      <SignupForm />
     </>
   );
 }
