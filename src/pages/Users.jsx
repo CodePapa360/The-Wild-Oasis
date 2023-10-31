f685ad37d3c49e9700f827eeaf1ab7b4100ac3d4
codepapa <codepapa360@gmail.com>
2023-10-31 21:08:57 +0600

    dual test git

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,11 +1,11 @@
-// import SignupForm from "../features/authentication/SignupForm";
+import SignupForm from "../features/authentication/SignupForm";
 import Heading from "../ui/Heading";
 
 function NewUsers() {
   return (
     <>
       <Heading as="h1">Create a new user</Heading>
-      {/* <SignupForm /> */}
+      <SignupForm />
     </>
   );
 }
