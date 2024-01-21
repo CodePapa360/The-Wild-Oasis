72857dac685231c05cd48f3126130965accec289
codepapa360 <codepapa360@gmail.com>
2024-01-21 15:57:22 +0600

    Add dotenv package and update Supabase configuration

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://qfvpyreitzgxuimnvjpy.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmdnB5cmVpdHpneHVpbW52anB5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc5NjE2NDQsImV4cCI6MjAxMzUzNzY0NH0.mWghnxwg5nWbtZmYPaqqncJxOUyuOMc4Evr02vUKKFI";
+export const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+// import process from "process";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +12,8 @@ export default defineConfig({
     open: true,
     port: 3000,
     host: "0.0.0.0",
+  },
+  define: {
+    "process.env": process.env,
   },
 });
